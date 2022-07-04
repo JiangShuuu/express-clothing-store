c4dcfc12acf7a6541c789ed8748c9d51b45cc16e
JiangShuuu <globelex65@gmail.com>
2022-07-04 11:03:04 +0800

    upd product

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -2,22 +2,22 @@ const productServices = require('../services/product-services')
 
 const productController = {
   getProducts: (req, res, next) => {
-    productServices.getProducts(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    productServices.getProducts(req, (err, data) => err ? next(err) : res.json({ status: 'success getAll', data }))
   },
   getProduct: (req, res, next) => {
-    productServices.getProduct(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    productServices.getProduct(req, (err, data) => err ? next(err) : res.json({ status: 'success get', data }))
   },
   postProduct: (req, res, next) => {
-    productServices.postProduct(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    productServices.postProduct(req, (err, data) => err ? next(err) : res.json({ status: 'success create', data }))
   },
   editProduct: (req, res, next) => {
-    productServices.editProduct(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    productServices.editProduct(req, (err, data) => err ? next(err) : res.json({ status: 'success getEdit', data }))
   },
   putProduct: (req, res, next) => {
-    productServices.putProduct(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    productServices.putProduct(req, (err, data) => err ? next(err) : res.json({ status: 'success edit', data }))
   },
   deleteProduct: (req, res, next) => {
-    productServices.deleteProduct(req, err => err ? next(err) : res.json({ status: 'success' }))
+    productServices.deleteProduct(req, err => err ? next(err) : res.json({ status: 'success del' }))
   }
 }
 
