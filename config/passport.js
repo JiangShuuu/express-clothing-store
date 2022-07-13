3277fa04c9f65bbe90270be90ebb4fa22d38d559
JiangShuuu <globelex65@gmail.com>
2022-07-13 10:31:40 +0800

    add admin auth

--- config/passport.js
+++ config/passport.js
@@ -19,25 +19,4 @@ passport.use(new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
     .catch(err => cb(err))
 }))
 
-// set up Passport strategy
-passport.use(new LocalStrategy(
-  // customize user field
-  {
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-  },
-  // authenticate user
-  (req, email, password, cb) => {
-    User.findOne({ where: { email } })
-      .then(user => {
-        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
-        bcrypt.compare(password, user.password).then(res => {
-          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
-          return cb(null, user)
-        })
-      })
-  }
-))
-
 module.exports = passport
