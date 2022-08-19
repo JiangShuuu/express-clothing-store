38fd07030ce3429b60e32d621f0bf5a81af3821d
JiangShuuu <globelex65@gmail.com>
2022-08-19 22:26:50 +0800

    upd thirdpartylogin

--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const db = require('../../models')
 const { User } = db
 
@@ -9,7 +10,10 @@ router.post('/third-party-login', ((req, res) => {
 
   User.findOne({ where: { email } })
       .then(user => {
-        if (user) return res.json({ status: 'success 成功登入', user })
+        const userData = user.toJSON()
+        const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' }) // 簽發 JWT，效期為 30 天
+        
+        if (user) return res.json({ status: 'success 成功登入', data:{ user, token } })
         const randomPassword = Math.random().toString(36).slice(-8)
         bcrypt
           .genSalt(10)
@@ -20,7 +24,7 @@ router.post('/third-party-login', ((req, res) => {
             password: hash
           }))
           .then(user => {
-            res.json({ status: 'success 取得使用者資料', user })
+            res.json({ status: 'success 取得使用者資料',  data:{ user, token } })
           })
           .catch(err => console.log(err))
       })
