61ac50e1690ad6d87e33c6d412657a3476522ba3
JiangShuuu <globelex65@gmail.com>
2022-09-14 10:01:15 +0800

    upd thirdpartylogin

--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -7,7 +7,7 @@ const { User, Product } = db
 
 router.post('/third-party-login', ((req, res) => {
   const { email, displayName } = req.body
-  
+
   User.findOne({ 
       where: { email },
       include: [
@@ -15,27 +15,28 @@ router.post('/third-party-login', ((req, res) => {
       ]
     })
       .then(user => {
-        
+        console.log(user)
         if (user) {
           const userData = user.toJSON()
           const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' }) // 簽發 JWT，效期為 30 天
           return res.json({ status: 'success 成功登入', data:{ userData: user, token } })
+        } else {
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash => User.create({
+              name: displayName,
+              email,
+              password: hash
+            }))
+            .then(user => {
+              const userData = user.toJSON()
+              const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
+              res.json({ status: 'success 取得使用者資料',  data:{ userData: user, token } })
+            })
+            .catch(err => console.log(err))
         }
-
-        const randomPassword = Math.random().toString(36).slice(-8)
-        bcrypt
-          .genSalt(10)
-          .then(salt => bcrypt.hash(randomPassword, salt))
-          .then(hash => User.create({
-            name: displayName,
-            email,
-            password: hash
-          }))
-          .then(user => {
-            const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '30d' })
-            res.json({ status: 'success 取得使用者資料',  data:{ userData: user, token } })
-          })
-          .catch(err => console.log(err))
       })
 }))
 
