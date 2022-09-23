23d7b40577dae2f6d6fc862719c14b4b1c3bb259
JiangShuuu <globelex65@gmail.com>
2022-09-23 14:46:43 +0800

    add swagger

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const routes = require('./routes')
 const session = require('express-session')
 const SESSION_SECRET = 'secret'
 const path = require('path')
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json')
 
 app.use(cors())
 
@@ -21,6 +24,22 @@ app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: fals
 app.use(methodOverride('_method'))
 
 app.use('/upload', express.static(path.join(__dirname, 'upload')))
+
+// const options = {
+//   swaggerDefinition: {
+//     // 這邊會是你的api文件網頁描述
+//     info: {
+//       title: 'ec_web_demo API',
+//       version: '1.0.0',
+//       description: 'Generate ec_web_demo API document with swagger'
+//     }
+//   },
+//   // 這邊會是你想要產生的api文件檔案，我是直接讓swagger去列出所有controllers
+//   apis: ['./controllers/*.js']
+// };
+// const specs = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(routes)
 
