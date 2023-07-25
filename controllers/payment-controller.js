ffe5a61e2ba785919ce51b3a3860246955a19555
JiangShuuu <globelex65@gmail.com>
2023-07-25 22:58:11 +0800

    add payment

--- controllers/payment-controller.js
+++ controllers/payment-controller.js
@@ -1,6 +1,5 @@
-const paymentServices = require("../services/payment-services");
-
 const ecpay_payment = require("ecpay_aio_nodejs");
+const { Cart, Order, Orderlist } = require("../models");
 
 function generateRandomCode() {
   const randomCode = Array.from({ length: 20 }, () => Math.random().toString(36)[2]).join('');
@@ -44,14 +43,18 @@ const options = {
 
 const paymentController = {
   payment: (req, res, next) => {
+    const { name, phone, address, total } = req.body;
+    const userId = req.user.id;
+    console.log('gggg', name, phone, address, total, userId)
+
     const ecpay = new ecpay_payment(options);
     let base_param = {
       MerchantTradeNo: generateRandomCode(), //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
       MerchantTradeDate: getFormattedDateTime(), //ex: 2017/02/13 15:45:30
-      TotalAmount: `${generateRandomAmount()}`,
+      TotalAmount: `${total}`,
       TradeDesc: "測試交易描述1",
       ItemName: "測試商品等",
-      ReturnURL: "https://0707-60-251-45-137.ngrok-free.app/notify",
+      ReturnURL: "https://e771-122-100-73-148.ngrok-free.app/notify",
       ClientBackURL: "http://localhost:3000/cart/confirm",
       // ChooseSubPayment: '',
       // OrderResultURL: 'http://192.168.0.1/payment_result',
@@ -61,11 +64,11 @@ const paymentController = {
       // Remark: '交易備註',
       // HoldTradeAMT: '1',
       // StoreID: '',
-      // CustomField1: '',
-      // CustomField2: '',
-      // CustomField3: '',
-      // CustomField4: ''
-    }
+      CustomField1: `${name}`,
+      CustomField2: `${phone}`,
+      CustomField3: `${address}`,
+      CustomField4: `${userId}`,
+    };
     
     let inv_params = {}
     
@@ -78,7 +81,43 @@ const paymentController = {
   },
   notify: (req, res, next) => {
     console.log("Payment notification received:", req.body);
-    res.redirect('/4044')
+    const { CustomField1, CustomField2, CustomField3, CustomField4, TradeAmt } =
+      req.body;
+    const userId = +CustomField4;
+    Cart.findAll({
+      where: {
+        userId,
+      },
+    })
+      .then((cart) => {
+        if (cart.length < 1) {
+          error.code = 400;
+          error.message = "購物車沒有任何商品!";
+          return next(error);
+        }
+
+        Order.create({
+          name: CustomField1,
+          phone: CustomField2,
+          address: CustomField3,
+          total: TradeAmt,
+          userId,
+        }).then((order) => {
+          cart.map((item) => {
+            Orderlist.create({
+              orderId: order.id,
+              productId: item.productId,
+              productCount: item.productCount,
+            });
+            item.destroy();
+          });
+        });
+      })
+      .then(() => res.json({ status: 200, msg: "成功新增訂單" }))
+      .catch((error) => {
+        error.code = 500;
+        next(error);
+      });
   }
 };
 
